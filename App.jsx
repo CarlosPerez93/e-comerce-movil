de08c60db393b23ca6d6c07d541258b0c2fd66f1
CarlosPerez93 <carlos.perez3@outlook.es>
2024-02-05 13:15:45 +0100

    feat(gesture): gesture libraries for emoji size

--- App.jsx
+++ App.jsx
@@ -1,5 +1,18 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { PickImageAsync } from "./src/components/PickImageAsync/PickImageasync";
+import { StyleSheet } from "react-native";
 
 export default function App() {
-  return <PickImageAsync />;
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <PickImageAsync />
+    </GestureHandlerRootView>
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+});
